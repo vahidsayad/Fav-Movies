77e9cbe84b925bfd8b6d6c07f2ad18eb6002eebc
Vahid Sayad <vahid.sayad@gmail.com>
2025-03-11 16:09:56 +0330

    Initial Commit

---
+++ Fav Movies/Fav_MoviesApp.swift
@@ -0,0 +1,32 @@
+//
+//  Fav_MoviesApp.swift
+//  Fav Movies
+//
+//  Created by Vahid Sayad on 11/03/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Fav_MoviesApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
